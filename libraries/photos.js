01d82dce76aa63959d4e4ff183620d7ba81338e9
Hazel Windle <hwindle83@googlemail.com>
2023-02-13 18:22:04 +0000

    Fixed API data response issue

--- libraries/photos.js
+++ libraries/photos.js
@@ -1,28 +1,31 @@
-const superagent = require('superagent');
+const axios = require('axios');
 require('dotenv').config();
 
 class Photo {
   constructor(oneImageObj) {
-    const {user: name, urls, description} = oneImageObj;
+    const { user: name, urls, description } = oneImageObj;
     this.name = name.name;
     this.imageUrl = urls.regular;
     this.description = description;
   }
 }
 
-
 const photoHandler = async (req, res) => {
   // get query parameter title
   const title = req.query.title;
-  console.log(title);
   const key = process.env.UNSPLASH_API_KEY;
   const url = `https://api.unsplash.com/search/photos?query=${title}&client_id=${key}`;
-
+  //console.log(url);
   try {
-    // can also add to superagent (headers) .set('Authorization', `Client-ID ${process.env.UNSPLASH_API_KEY}`)
-    const imgData = await superagent.get(url);
-    const catArray = await imgData.results.map((cat) => new Photo(cat));
-    res.status(200).send(catArray);
+    // can also add following to headers: ('Authorization', `Client-ID ${process.env.UNSPLASH_API_KEY}`)
+    await axios
+      .get(url)
+      .then((imgData) => {
+        // console.dir(imgData);
+        const catArray = imgData.data.results.map((cat) => new Photo(cat));
+        return res.status(200).send(catArray);
+      })
+      .catch((err) => console.log(err));
   } catch {
     res.status(500).send('API fetching problem');
   }
@@ -31,11 +34,13 @@ const photoHandler = async (req, res) => {
 const randomPicHandler = async (req, res) => {
   const key = process.env.UNSPLASH_API_KEY;
   const url = `https://api.unsplash.com/photos/random?client_id=${key}`;
-  console.log('In random pics');
+
   try {
-    const imgData = await superagent.get(url);
-    const photoObject = new Photo(imgData);
-    res.status(200).send(photoObject);
+    await axios.get(url).then(randomData => {
+      // console.dir(randomData);
+      const photoObject = new Photo(randomData.data);
+      return res.status(200).send(photoObject);
+    }).catch((err) => console.log(err));
   } catch {
     res.status(500).send('API fetching problem');
   }
